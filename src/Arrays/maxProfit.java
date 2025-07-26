53869b08ea602d69285da9957eb18ccc06fb8c62
Kaif <kaifmumtajansari@gmail.com>
2025-07-26 18:48:07 +0530

    Added new problem - maxProfit

---
+++ src/Arrays/maxProfit.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class maxProfit {
+    public static int buyAndSell(int[] nums){
+        int n = nums.length;
+        int buy = nums[0];
+        int profit = 0;
+        for(int i = 1; i < n; i++){
+            if(nums[i] > buy){
+                buy = nums[i];
+            } else if(nums[i] - buy > profit){
+                profit = nums[i] - buy;
+            }
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,1,9};
+        System.out.println("Maximum Profit : " + buyAndSell(nums));
+    }
+}
