565e92a8394efc3246afaeb1d42ad74adbe98528
Mohammad Kaif Ansari <kaifmumtajansari@gmail.com>
2025-07-27 13:13:34 +0530

    Update FindNumbers.java

--- src/Arrays/FindNumbers.java
+++ src/Arrays/FindNumbers.java
@@ -1,4 +1,22 @@
 package Arrays;
 
+import java.util.ArrayList;
+
 public class FindNumbers {
+    public static ArrayList findNumbers(int[] nums){
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = nums.length;
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i - 1]){
+                list.add(nums[i]);
+                list.add(nums[i] + 1);
+            }
+        }
+
+        return list;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,5,7,8};
+        System.out.println(findNumbers(nums));
+    }
 }
