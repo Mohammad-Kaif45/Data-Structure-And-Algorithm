68757200bc381decd6a504c10af31c3f0120d724
Kaif <kaifmumtajansari@gmail.com>
2025-07-27 18:54:21 +0530

    New problem

---
+++ src/Arrays/ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class ContainerWithMostWater {
+    public static int findContainer(int[] nums){
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int maxWater = 0;
+        while(left < right){
+            int h = Math.min(nums[left],nums[right]);
+            int width = right - left;
+            int area = h * width;
+            maxWater = Math.max(maxWater,area);
+            if(nums[left] < nums[right]){
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,8,6,2,5,4,8,3,7};
+        System.out.println(findContainer(nums));
+    }
+}
--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -1,4 +1,23 @@
 package Arrays;
-
+import java.util.Arrays;
 public class TwoSum {
+        public static int[] findTwoSum(int[] arr,int target) {
+        int n = arr.length;
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] + arr[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,4,7,2,6};
+        System.out.println(Arrays.toString(findTwoSum(nums, 9)));
+    }
 }
