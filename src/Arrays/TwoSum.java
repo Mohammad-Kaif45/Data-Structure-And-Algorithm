6a9e3878b0b82691467f199239759032213db568
Mohammad Kaif Ansari <kaifmumtajansari@gmail.com>
2025-07-27 13:12:30 +0530

    Update TwoSum.java

--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -2,7 +2,7 @@ package Arrays;
 import java.util.Arrays;
 public class TwoSum {
         public static int[] findTwoSum(int[] arr,int target) {
-        int n = arr.length;
+        int n = arr.length; 
         int[] ans = new int[2];
         ans[0] = ans[1] = -1;
         for(int i = 0; i < n; i++) {
