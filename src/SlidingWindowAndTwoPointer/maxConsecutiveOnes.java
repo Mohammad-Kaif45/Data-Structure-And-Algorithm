d06726b4379489d561dffda7992398465636a50b
Mohammad Kaif Ansari <kaifmumtajansari@gmail.com>
2025-07-29 22:53:59 +0530

    Update maxConsecutiveOnes.java

--- src/SlidingWindowAndTwoPointer/maxConsecutiveOnes.java
+++ src/SlidingWindowAndTwoPointer/maxConsecutiveOnes.java
@@ -6,7 +6,7 @@ public class maxConsecutiveOnes {
         int n = nums.length;
         for(int i = 0; i < n; i++){
             int zeros = 0;
-            for(int j = i; j < n; j++){
+            for(int j = i; j < n; j++){ 
                 if(nums[j] == 0){
                     zeros++;
                 }
