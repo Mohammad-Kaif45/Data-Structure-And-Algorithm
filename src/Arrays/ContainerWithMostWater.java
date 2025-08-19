083ceaaf1e1566dc0c6da0e2717e2eaa42dbcbf6
Mohammad Kaif Ansari <kaifmumtajansari@gmail.com>
2025-08-19 12:49:08 +0530

    Update ContainerWithMostWater.java

--- src/Arrays/ContainerWithMostWater.java
+++ src/Arrays/ContainerWithMostWater.java
@@ -1,7 +1,7 @@
 package Arrays;
 
 public class ContainerWithMostWater {
-    public static int findContainer(int[] nums){ 
+    public static int findContainer(int[] nums){  
         int n = nums.length;
         int left = 0;
         int right = n - 1;
